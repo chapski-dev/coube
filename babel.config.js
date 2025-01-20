22c6029fa3ed62d12a7f46e5210065ae00283b83
Alexey Chapski <alexey.chapski@gmail.com>
2025-01-20 16:00:24 +0300

    feat: imoplement babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+const moduleResolver = [
+  require.resolve('babel-plugin-module-resolver'),
+  {
+    root: ['./'],
+    alias: {
+      '@src': './src',
+      '@assets': './assets',
+    },
+  },
+];
+
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [moduleResolver],
+
 };
