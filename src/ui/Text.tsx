04ae212d02014b2a9aa06b460b62b818196746ba
Alexey Chapski <alexey.chapski@gmail.com>
2025-02-25 17:15:38 +0300

    fix(ui/Text): set textDefault color for any theme

--- src/ui/Text.tsx
+++ src/ui/Text.tsx
@@ -1,48 +1,55 @@
 import React, { FC, useMemo } from 'react';
-import {
-  StyleSheet,
-  Text as RNText,
-  TextProps as RNTextProps,
-  TextStyle,
-} from 'react-native';
+import { StyleSheet, Text as RNText, TextProps as RNTextProps, TextStyle } from 'react-native';
 
+import { useAppTheme } from '@src/theme/theme';
 import { removeUndefinedOnes } from '@src/utils';
 
 type TextType = keyof typeof styles;
-type Spacing = number
+type Spacing = number;
 
-type TextProps = RNTextProps & {
-  type?: TextType;
-  color?: string;
-  fontWeight?: TextStyle['fontWeight'];
-  fontSize?: number;
-  center?: boolean;
-  left?: boolean;
-  right?: boolean;
-  uppercase?: boolean;
-  lowercase?: boolean;
-  capitalize?: boolean;
-  m?: Spacing
-  mx?: Spacing
-  my?: Spacing
-  mt?: Spacing
-  mr?: Spacing
-  mb?: Spacing
-  ml?: Spacing
-  p?: Spacing
-  px?: Spacing
-  py?: Spacing
-  pt?: Spacing
-  pr?: Spacing
-  pb?: Spacing
-  pl?: Spacing
+export type colorKeys = keyof ReturnType<typeof useAppTheme>['colors'];
+
+type ThemeProps = {
+  lightColorName?: colorKeys;
+  darkColorName?: colorKeys;
+  colorName?: colorKeys;
 };
 
-// TODO: add theme colors
+type TextProps = RNTextProps &
+  ThemeProps & {
+    type?: TextType;
+    color?: string;
+    fontWeight?: TextStyle['fontWeight'];
+    fontSize?: number;
+    center?: boolean;
+    left?: boolean;
+    right?: boolean;
+    uppercase?: boolean;
+    lowercase?: boolean;
+    capitalize?: boolean;
+    m?: Spacing;
+    mx?: Spacing;
+    my?: Spacing;
+    mt?: Spacing;
+    mr?: Spacing;
+    mb?: Spacing;
+    ml?: Spacing;
+    p?: Spacing;
+    px?: Spacing;
+    py?: Spacing;
+    pt?: Spacing;
+    pr?: Spacing;
+    pb?: Spacing;
+    pl?: Spacing;
+  };
+
 const Text: FC<TextProps> = ({
   type,
   style,
   color,
+  colorName,
+  // lightColorName,
+  // darkColorName,
   fontWeight,
   fontSize,
   left,
@@ -67,13 +74,25 @@ const Text: FC<TextProps> = ({
   pl,
   ...rest
 }) => {
+  const theme = useAppTheme();
+  // const key = colorName || (theme.dark ? darkColorName : lightColorName) || 'textDefault';
+  const key = colorName || 'textDefault';
+
+
   const computedStyle = useMemo(() => {
-    const textAlign = left ? 'left' : right ? 'right' : center ? 'center' : undefined
-    const textTransform = uppercase ? 'uppercase' : lowercase ? 'lowercase' : capitalize ? 'capitalize' : undefined
+    const textAlign = left ? 'left' : right ? 'right' : center ? 'center' : undefined;
+    const textTransform = uppercase
+      ? 'uppercase'
+      : lowercase
+        ? 'lowercase'
+        : capitalize
+          ? 'capitalize'
+          : undefined;
 
     return [
       styles[type ? type : 'body'],
       {
+        color: theme.colors[key],
         ...removeUndefinedOnes({
           color,
           fontSize,
@@ -93,42 +112,70 @@ const Text: FC<TextProps> = ({
           paddingTop: pt,
           paddingVertical: py,
           textAlign,
-          textTransform,
+          textTransform
         })
       },
       style
     ].flat();
-  }, [capitalize, center, color, fontSize, fontWeight, left, lowercase, m, mb, ml, mr, mt, mx, my, p, pb, pl, pr, pt, px, py, right, style, type, uppercase]);
+  }, [
+    capitalize,
+    center,
+    color,
+    fontSize,
+    fontWeight,
+    key,
+    left,
+    lowercase,
+    m,
+    mb,
+    ml,
+    mr,
+    mt,
+    mx,
+    my,
+    p,
+    pb,
+    pl,
+    pr,
+    pt,
+    px,
+    py,
+    right,
+    style,
+    theme.colors,
+    type,
+    uppercase
+  ]);
 
   return <RNText {...rest} style={computedStyle} />;
 };
 
 const styles = StyleSheet.create({
   body: {
-    fontSize: 14,
+    fontSize: 14
   },
   body_500: {
     color: '#000',
     fontSize: 15,
-    fontWeight: '500',
+    fontWeight: '500'
   },
   h1: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   h2: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   h3: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   label: {
     color: '#798391',
     fontSize: 13,
-    lineHeight: 16,
-  },
+    lineHeight: 16
+  }
 });
 
-export {Text};
+export { Text };
