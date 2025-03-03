427f5b64d30f6780a195268746a79add8c169d13
Alexey Chapski <alexey.chapski@gmail.com>
2025-03-03 16:39:38 +0300

    chore(useAppColorTheme): set static color scheme

--- src/hooks/useAppColorTheme.ts
+++ src/hooks/useAppColorTheme.ts
@@ -1,30 +1,30 @@
 // import { useColorScheme } from 'react-native';
-import { AppDarkTheme, AppLightTheme } from '@src/theme/theme';
+
+import { AppLightTheme } from '@src/theme/theme';
 import { ASYNC_STORAGE_KEYS } from '@src/vars/async_storage_keys';
 
 import { useCustomAsyncStorage } from './useAsyncStorage';
-
+// TODO: return in the future to changeble theme
 export const useAppColorTheme = () => {
   const [appThemeKey] = useCustomAsyncStorage(ASYNC_STORAGE_KEYS.ThemeKey);
   // const deviceColorScheme = useColorScheme();
 
-  let theme: Omit<App.Theme, 'insets'> = AppLightTheme;
+  const theme: Omit<App.Theme, 'insets'> = AppLightTheme;
 
-  switch (appThemeKey) {
-    case 'dark': {
-      theme = AppDarkTheme;
-      break;
-    }
-    case 'light': {
-      theme = AppLightTheme;
-      break;
-    }
-    default: {
-      // theme = deviceColorScheme === 'dark' ? AppDarkTheme : AppLightTheme;
-      theme = AppLightTheme;
-      break;
-    }
-  }
+  // switch (appThemeKey) {
+  //   case 'dark': {
+  //     theme = AppDarkTheme;
+  //     break;
+  //   }
+  //   case 'light': {
+  //     theme = AppLightTheme;
+  //     break;
+  //   }
+  //   default: {
+  //     theme = deviceColorScheme === 'dark' ? AppDarkTheme : AppLightTheme;
+  //     break;
+  //   }
+  // }
 
   return { appThemeKey, theme };
 };
